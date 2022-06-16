1f74ff7ea42fe03cf5108d1463cda126bf337bb1
Andrea Gonzalez <andreagonz@fb.com>
2022-06-16 13:03:36 -0700

    Initial Commit

---
+++ Flixter2Tests/Flixter2Tests.m
@@ -0,0 +1,36 @@
+//
+//  Flixter2Tests.m
+//  Flixter2Tests
+//
+//  Created by Andrea Gonzalez on 6/16/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flixter2Tests : XCTestCase
+
+@end
+
+@implementation Flixter2Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Flixter2UITests/Flixter2UITestsLaunchTests.m
@@ -0,0 +1,37 @@
+//
+//  Flixter2UITestsLaunchTests.m
+//  Flixter2UITests
+//
+//  Created by Andrea Gonzalez on 6/16/22.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Flixter2UITestsLaunchTests : XCTestCase
+
+@end
+
+@implementation Flixter2UITestsLaunchTests
+
++ (BOOL)runsForEachTargetApplicationUIConfiguration {
+    return YES;
+}
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+- (void)testLaunch {
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [app launch];
+
+    // Insert steps here to perform after app launch but before taking a screenshot,
+    // such as logging into a test account or navigating somewhere in the app
+
+    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    attachment.name = @"Launch Screen";
+    attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
+    [self addAttachment:attachment];
+}
+
+@end
